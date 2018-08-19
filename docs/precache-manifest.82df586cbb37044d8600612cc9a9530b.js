586457a87d0a32ec08a96917df80c7fd54c8389f
lucaslos <lucas@sugestly.com>
2018-08-19 01:20:41 -0300

    change folder name

---
+++ docs/precache-manifest.82df586cbb37044d8600612cc9a9530b.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "42d8e1b27d218b4575fc",
+    "url": "style.css"
+  },
+  {
+    "revision": "42d8e1b27d218b4575fc",
+    "url": "bundle.js"
+  }
+];
