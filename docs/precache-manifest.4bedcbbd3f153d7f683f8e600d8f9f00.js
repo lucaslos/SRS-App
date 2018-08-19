dbcb185bcd9f2d3390682800ab8c1040ea87b0ae
lucaslos <lucas@sugestly.com>
2018-08-19 19:36:05 -0300

    SRS App v3.0.2

---
+++ docs/precache-manifest.4bedcbbd3f153d7f683f8e600d8f9f00.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "2099a45e74d5bffcdb47",
+    "url": "style.css"
+  },
+  {
+    "revision": "2099a45e74d5bffcdb47",
+    "url": "bundle.js"
+  }
+];
--- docs/sw.js
+++ docs/sw.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.500b829fe47d669d9fdafa9b223a88ea.js"
+  "precache-manifest.4bedcbbd3f153d7f683f8e600d8f9f00.js"
 );
 
 workbox.skipWaiting();
