f91b1a9fe2dd141e8ecdfae75c557af6af21b260
lucaslos <lucas@sugestly.com>
2019-01-27 23:41:15 -0200

    Change file name

--- scripts/create-icon-set.js
+++ scripts/create-icon-set.js
@@ -1,21 +1,53 @@
+/* eslint-disable quote-props */
 /* react icon set creator v1.0.0 */
 
 const fs = require('fs');
 const path = require('path');
 const clipboardy = require('clipboardy');
 const SVGO = require('svgo-sync');
+const currentSet = require('../src/data/icons.json');
 
 // TODO: merge sets
 
 /* settings */
 const inputFolder = '../resources/Navigation Icons/';
-const outputFile = '../src/data/iconsStore.json';
+const outputFile = '../src/data/icons.json';
 const input = {
-  logo: `
-  <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-  <path fill-rule="evenodd" clip-rule="evenodd" d="M18 10H34.7068C35.5799 10 36.0337 11.0406 35.4396 11.6805L24.2967 23.6805C24.1075 23.8842 23.842 24 23.5639 24H18C14.134 24 11 20.866 11 17C11 13.134 14.134 10 18 10Z" fill="#00FFC2"/>
-  <path opacity="0.5" fill-rule="evenodd" clip-rule="evenodd" d="M30 24H24.4361C24.158 24 23.8925 24.1158 23.7033 24.3195L12.5604 36.3195C11.9663 36.9594 12.4201 38 13.2932 38H30C33.866 38 37 34.866 37 31C37 27.134 33.866 24 30 24Z" fill="#00FFC2"/>
+  warn: `
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M1 21H23L12 2L1 21ZM13 18H11V16H13V18ZM13 14H11V10H13V14Z" fill="#E53558"/>
   </svg>
+  `,
+  check: `
+  <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M12.0002 21.56L6.44021 16L4.54688 17.88L12.0002 25.3333L28.0002 9.33334L26.1202 7.45334L12.0002 21.56Z" fill="#E5DE35"/>
+  </svg>
+  `,
+  'double-check': `
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M10.59 16.58L6.42 12.41L5 13.82L10.59 19.41L22.59 7.41L21.18 6L10.59 16.58Z" fill="#00FFC2"/>
+  <path opacity="0.5" d="M6.59 16.17L2.42 12L1 13.41L6.59 19L18.59 7.00003L17.18 5.59003L6.59 16.17Z" fill="#00FFC2"/>
+  </svg>
+  `,
+  'more-vert': `
+  <svg width="18" height="24" viewBox="0 0 18 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M12 8C13.1 8 14 7.1 14 6C14 4.9 13.1 4 12 4C10.9 4 10 4.9 10 6C10 7.1 10.9 8 12 8ZM12 10C10.9 10 10 10.9 10 12C10 13.1 10.9 14 12 14C13.1 14 14 13.1 14 12C14 10.9 13.1 10 12 10ZM10 18C10 16.9 10.9 16 12 16C13.1 16 14 16.9 14 18C14 19.1 13.1 20 12 20C10.9 20 10 19.1 10 18Z" fill="#00FFC2"/>
+  </svg>
+
+  `,
+  'sound': `
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" clip-rule="evenodd" d="M3 15V9H7L12 4V20L7 15H3ZM10 15.17V8.83L7.83 11H5V13H7.83L10 15.17Z" fill="#00FFC2"/>
+  <path d="M14 7.96997V16.02C15.48 15.29 16.5 13.77 16.5 12C16.5 10.23 15.48 8.70997 14 7.96997Z" fill="#00FFC2"/>
+  <path d="M14 3.22998V5.28998C16.89 6.14998 19 8.82998 19 12C19 15.17 16.89 17.85 14 18.71V20.77C18.01 19.86 21 16.28 21 12C21 7.71998 18.01 4.13998 14 3.22998Z" fill="#00FFC2"/>
+  </svg>
+
+  `,
+  'arrow-back': `
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M20 11H7.83L13.42 5.41L12 4L4 12L12 20L13.41 18.59L7.83 13H20V11Z" fill="#00FFC2"/>
+</svg>
+
   `,
 };
 const renames = {
