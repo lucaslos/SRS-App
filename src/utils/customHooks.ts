f8792782bcd037b4224a41924d6ba8caa0136178
lucaslos <lucas@sugestly.com>
2019-02-01 00:34:23 -0200

    Add useThrottle hook

--- src/utils/customHooks.ts
+++ src/utils/customHooks.ts
@@ -1,9 +1,4 @@
-import {
-  useState,
-  useRef,
-  useCallback,
-  useEffect,
-} from 'react';
+import { useState, useRef, useCallback, useEffect } from 'react';
 
 // export const useClickOutside = (callBack) => {
 //   const ref = useRef(null);
@@ -85,3 +80,31 @@ export function useOnChange<T>(value: T, callBack: (last: T) => void) {
 //     setLast(value);
 //   }
 // }
+
+/**
+ *
+ * @param value value to be debounced
+ * @param limit limit in ms
+ */
+export const useThrottle = <T>(value: T, limit: number) => {
+  const [throttledValue, setThrottledValue] = useState(value);
+  const lastRan = useRef(Date.now());
+
+  useEffect(
+    () => {
+      const handler = setTimeout(() => {
+        if (Date.now() - lastRan.current >= limit) {
+          setThrottledValue(value);
+          lastRan.current = Date.now();
+        }
+      }, limit - (Date.now() - lastRan.current));
+
+      return () => {
+        clearTimeout(handler);
+      };
+    },
+    [value, limit]
+  );
+
+  return throttledValue;
+};
