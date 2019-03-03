ac3e6f00a055d87a68f17d0fbdf808705531115e
lucaslos <lucas@sugestly.com>
2019-03-03 18:47:15 -0300

    Add import from gt feature

--- src/containers/AddCardsModal.tsx
+++ src/containers/AddCardsModal.tsx
@@ -1,21 +1,21 @@
+import css from '@emotion/css';
 import styled from '@emotion/styled';
 import Button from 'components/Button';
+import CardTile, { TilesWrapper } from 'components/CardTile';
 import Modal, {
   bottomButtonsWrapperStyle,
   BoxCloseButton,
   boxStyle,
 } from 'components/Modal';
 import AddNewCardModal from 'containers/AddNewCardModal';
+import EditCardModal from 'containers/EditCardModal';
 import React, { useState } from 'react';
+import cardsState, { pushNewCards } from 'state/cards';
 import modalsState from 'state/modals';
-import reviewState from 'state/review';
 import { centerContent } from 'style/modifiers';
 import { colorSecondary, colorYellow, fontDecorative } from 'style/theme';
-import CardTile, { TilesWrapper } from 'components/CardTile';
-import EditCardModal from 'containers/EditCardModal';
 import { replaceAt } from 'utils/genericUtils';
-import cardsState, { pushNewCards } from 'state/cards';
-import css from '@emotion/css';
+import { filterCardsFromGoogleTranslate } from 'utils/googleTranslate';
 
 const AddCardsButtonsWrapper = styled.div`
   ${centerContent};
@@ -28,12 +28,27 @@ const AddCardsButtonsWrapper = styled.div`
   }
 `;
 
+const Warn = styled.div`
+  margin-top: 12px;
+  width: 100%;
+  text-align: center;
+  margin-bottom: 8px;
+  font-size: 16px;
+  font-weight: 300;
+  color: ${colorYellow};
+
+  strong {
+    font-family: ${fontDecorative};
+  }
+`;
+
 const AddCardsModal = () => {
   const [show, setShow] = modalsState.useStore('addCards');
   const [cardsToAdd, setCardsToAdd] = useState<Card[]>([]);
   const [showAddCard, setShowAddCard] = useState<boolean>(false);
   const [showEditCard, setShowEditCard] = useState<boolean>(false);
   const [editCardId, setEditCardId] = useState<number>(0);
+  const [importError, setImportError] = useState('');
 
   function handleSaveCards() {
     pushNewCards(cardsToAdd);
@@ -62,11 +77,41 @@ const AddCardsModal = () => {
     setShowEditCard(true);
   }
 
-  const frontIsDuplicated = cardsToAdd.filter(newCard =>
-    cardsState
-      .getState()
-      .cards.some(card => card.front.trim() === newCard.front.trim())
-  );
+  function importFromTranslate() {
+    setImportError('');
+
+    navigator.clipboard
+      .readText()
+      .then(text => {
+        const importedCards = filterCardsFromGoogleTranslate(text);
+
+        if (importedCards) {
+          setCardsToAdd(importedCards);
+        } else {
+          setImportError('Copied content is invalid');
+        }
+      })
+      .catch(err => {
+        setImportError('Failed to read clipboard contents');
+        console.error('Failed to read clipboard contents: ', err);
+      });
+  }
+
+  const frontIsDuplicated = cardsState
+    .getState()
+    .cards.filter(card =>
+      cardsToAdd.some(newCard => card.front.trim() === newCard.front.trim())
+    );
+
+  const backIsDuplicated = cardsState
+    .getState()
+    .cards.filter(card =>
+      cardsToAdd.some(newCard => card.back.trim() === newCard.back.trim())
+    );
+
+  function showCardDialog(card: Card) {
+    alert(`front: ${card.front} \nback: ${card.back}`);
+  }
 
   return (
     <>
@@ -89,30 +134,61 @@ const AddCardsModal = () => {
             ))}
           </TilesWrapper>
           {frontIsDuplicated.length > 0 && (
-            <div
-              css={css`
-                margin-top: 12px;
-                margin-bottom: 8px;
-                font-size: 16px;
-                font-weight: 300;
-                color: ${colorYellow};
-
-                strong {
-                  font-family: ${fontDecorative};
-                }
-              `}
-            >
+            <Warn>
               Duplicated front text:{' '}
               <strong>
-                {frontIsDuplicated
-                  .map(card =>
-                    (card.front.length > 30
+                {frontIsDuplicated.map((card, i) => (
+                  <span
+                    key={i}
+                    onClick={() => showCardDialog(card)}
+                    css={{
+                      cursor: 'pointer',
+                      '&:after': {
+                        content: ',',
+                      },
+                      '&:last-of-type:after': {
+                        content: '',
+                      },
+                    }}
+                  >
+                    {card.front.length > 30
                       ? `${card.front.substr(0, 30)}...`
-                      : card.front)
-                  )
-                  .join(', ')}
+                      : card.front}
+                  </span>
+                ))}
               </strong>
-            </div>
+            </Warn>
+          )}
+          {backIsDuplicated.length > 0 && (
+            <Warn>
+              Duplicated back text:{' '}
+              <strong>
+                {backIsDuplicated.map((card, i) => (
+                  <span
+                    key={i}
+                    onClick={() => showCardDialog(card)}
+                    css={{
+                      cursor: 'pointer',
+                      '&:after': {
+                        content: ',',
+                      },
+                      '&:last-of-type:after': {
+                        content: '',
+                      },
+                    }}
+                  >
+                    {card.back.length > 30
+                      ? `${card.back.substr(0, 30)}...`
+                      : card.back}
+                  </span>
+                ))}
+              </strong>
+            </Warn>
+          )}
+          {importError && (
+            <Warn>
+              <strong>{importError}</strong>
+            </Warn>
           )}
           <AddCardsButtonsWrapper>
             <Button
@@ -122,10 +198,10 @@ const AddCardsModal = () => {
               onClick={() => setShowAddCard(true)}
             />
             <Button
-              label="paste cards"
+              label="Paste Cards"
               color={colorSecondary}
               smallRound
-              onClick={handleClose}
+              onClick={importFromTranslate}
             />
           </AddCardsButtonsWrapper>
           <div css={bottomButtonsWrapperStyle}>
---
+++ src/utils/googleTranslate.ts
@@ -0,0 +1,55 @@
+import cardsState from 'state/cards';
+import removeMd from 'remove-markdown';
+
+const wordsLimit = 20;
+
+function parseJSON<T = any>(string: string): T | false {
+  try {
+    return JSON.parse(string);
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+}
+
+export function filterCardsFromGoogleTranslate(pastedContent: string) {
+  const { cards } = cardsState.getState();
+
+  const words = parseJSON<{ front: string; back: string }[]>(pastedContent);
+
+  if (!words || !words[0] || !words[0].back) return false;
+
+  const newCards: Card[] = [];
+
+  for (let i = 0; i < wordsLimit; i++) {
+    const word = words[i];
+    const backLines = word.back.split('\n');
+    const back = (backLines.length === 1 ? backLines[0] : backLines[1])
+      .trim()
+      .toLowerCase();
+    const front = word.front
+      .replace('#', '')
+      .trim()
+      .toLowerCase();
+
+    if (
+      !cards.some(card => card.front === front && card.back === back)
+    ) {
+      newCards.push({
+        id: '0',
+        back,
+        front,
+        notes: [],
+        tags: [],
+        diff: 0,
+        lang: 'en',
+        wrongReviews: 0,
+        repetitions: 0,
+      });
+    } else {
+      console.log(`${word.front}|${word.back} is duplicated.`);
+    }
+  }
+
+  return newCards;
+}
